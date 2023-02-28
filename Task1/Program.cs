6176ecc1077f3fcb6a5869b0aa9fc2579ed95a14
BertieWoo88 <050688V@gmail.com>
2023-02-28 15:06:12 +0300

    tasks added

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+
+// string[] st = Console.ReadLine().Split(' ');
+// int A = int.Parse(st[0]);
+// int B = int.Parse(st[1]);
+// int C = int.Parse(st[2]);
+// int A = asknum("A");
+// int B = asknum("B");
+// int C = asknum("C");
+// Trianglecheck(A, B, C);
+
+// int asknum (string name)
+// {
+//     Console.Write($"Введите число {name}: ");
+//     int n = int.Parse(Console.ReadLine()!);
+//     return n; 
+// }
+Console.Write ("Введите числа через пробел: ");
+string [] st = Console.ReadLine().Split(' ');
+
+Console.WriteLine(sumPlus(st));
+
+int sumPlus (string[] array)
+{
+int count = 0;
+    foreach ( string el in array)
+{ 
+ if ( int.Parse(el)>0) count++; }
+return count;
+}
+ 
---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку 
+пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+double b1 = asknum("b1");
+double k1 = asknum("k1");
+double b2 = asknum("b2");
+double k2 = asknum("k2");
+
+double[] crospoint = getcross(b1, k1, b2, k2);
+
+Console.WriteLine($"точка пересечения графиков {k1}*X + {b1} и {k2}*X + {b2} -> ({String.Join(", ", crospoint)}) ");
+
+double asknum(string name)
+{
+    Console.Write($"Введите число {name}: ");
+    double n = double.Parse(Console.ReadLine()!);
+    return n;
+}
+
+double[] getcross(double b1, double k1, double b2, double k2)
+{
+    double[] answ = new double[2];
+    answ[0] = (b2 - b1) / (k1 - k2);
+    answ[1] = (k1 * answ[0]) + b1;
+    return answ;
+}
+
+//Console.WriteLine($"{s:f2}"); // 2 знака после запятой
